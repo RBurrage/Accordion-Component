acec1f9f3dcd592621e220897d347c68e1d11605
Robin Burrage <robin.o.burrage@gmail.com>
2024-01-25 20:40:03 -0500

    Add variant that opens one accordion item  at a time -- Add variant that allows developers to use any content at all (jsx) in the accordion item

--- src/App.js
+++ src/App.js
@@ -29,18 +29,70 @@ function App() {
 export default App;
 
 const Accordion = ({ data }) => {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
-      {data.map((el, i) => <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />)}
+      {data.map((el, i) => (
+        // Accordion pulling data from object...
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}>
+          {el.text}
+        </AccordionItem>
+      ))}
+
+      {/* ...or accordions with configurable contents made possible by way of the children prop */}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title='Content Example Using the Children Prop (JSX)'
+        num={22}
+        key='test 1'>
+        <p>Allows developers to place the content of their choice into the accordion item.</p><br />
+        <p>Perhaps a table, with links</p>
+        <table>
+          <tr>
+            <td>One</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td><a href="https://www.google.com">Google</a></td>
+            <td>6</td>
+          </tr>
+        </table><br />
+        <p>Or an unordered list</p>
+        <ol>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </ol>
+      </AccordionItem>
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title='Image Example Using the Children Prop (JSX)'
+        num={53}
+        key='test 2'>
+        Allows developers to place the content of their choice into the accordion item.
+        <img src="https://i.etsystatic.com/45189060/r/il/43559f/5258237400/il_1588xN.5258237400_1vbg.jpg" alt="Golden Retriever Puppy" width="200" />
+      </AccordionItem>
+
     </div>
   )
 }
 
-const AccordionItem = ({ num, title, text }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const AccordionItem = ({ num, title, curOpen, onOpen, children }) => {
+  const isOpen = num === curOpen;
 
   const handleToggle = () => {
-    setIsOpen(!isOpen);
+    onOpen(isOpen ? null : num)
   }
 
   return (
@@ -48,8 +100,8 @@ const AccordionItem = ({ num, title, text }) => {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
-      { isOpen &&
-        <div className="content-box">{text}</div>
+      {isOpen &&
+        <div className="content-box">{children}</div>
       }
     </div>
   )
